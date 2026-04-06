9cc6d0c6663c99736f60eacf869bfce4555fb1a7
my-name-is-yu <yuyoshimuta1217@gmail.com>
2026-04-06 21:57:52 +0900

    feat(queue): add barrel index for queue module

---
+++ src/runtime/queue/index.ts
@@ -0,0 +1,5 @@
+export { PriorityQueue } from './priority-queue.js';
+export { EventBus } from './event-bus.js';
+export type { EventBusOptions } from './event-bus.js';
+export { CommandBus } from './command-bus.js';
+export type { CommandBusOptions } from './command-bus.js';
