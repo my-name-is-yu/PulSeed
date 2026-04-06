0d6c20cd1126c62f91f24a57a1aeea4db3e55603
my-name-is-yu <yuyoshimuta1217@gmail.com>
2026-04-06 23:32:05 +0900

    chore(gateway): export WsChannelAdapter from gateway index

--- src/runtime/gateway/index.ts
+++ src/runtime/gateway/index.ts
@@ -1,3 +1,5 @@
 export { IngressGateway } from "./ingress-gateway.js";
 export { HttpChannelAdapter } from "./http-channel-adapter.js";
 export type { ChannelAdapter, EnvelopeHandler, ReplyChannel } from "./channel-adapter.js";
+export { WsChannelAdapter } from "./ws-channel-adapter.js";
+export type { WsLike, WsSocketLike } from "./ws-channel-adapter.js";
